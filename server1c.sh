9b619a7a1ce343b603249274c8f51e3b0a730140
Evgeny Karamnov <94249274+karamn0v@users.noreply.github.com>
2025-11-01 13:46:01 +0300

    Update server1c.sh

--- server1c.sh
+++ server1c.sh
@@ -48,5 +48,21 @@ pct create $CTID $IMG \
 pct start $CTID
 sleep 5
 
-# --- Install TorrServer ---
-echo ">>> Installing server1c inside CT..."
+# --- Install server 1c ---
+echo ">>> Installing server 1c inside CT..."
+
+pct exec $CTID -- bash -c "
+  apt-get update && apt-get install -y wget curl gdebi ttf-mscorefonts-installer unzip libfreetype6 libgsf-1-common unixodbc glib2.0
+  fc-cache –fv
+  mkdir -p /1c/soft/1c
+  cd /1c/soft/1c
+  wget -qO https://1c.e-r-p.ru/deb64_8_3_27_1559.zip
+  unzip -q deb64_8_3_27_1559.zip
+  rm deb64_8_3_27_1559.zip
+  gdebi 1c-enterprise83-common_8.3.27-1559_amd64.deb
+  gdebi 1c-enterprise83-server_8.3.27-1559_amd64.deb
+  gdebi 1c-enterprise83-ws_8.3.27-1559_amd64.deb
+  systemctl start srv1cv83
+  "
+
+echo -e "\n>>> server 1c is installed!"
